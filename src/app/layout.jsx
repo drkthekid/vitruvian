71369c50a6d4bfc9de0aa6a26c3ae1ecea1dcf13
ramonespier <ramon.coelhomelo@outlook.com>
2026-04-23 09:40:50 -0300

    feat: click spark

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,13 +2,14 @@ import Navbar from "@/components/features/nav/navbar";
 import "./globals.css";
 import { ThemeProvider } from "@/components/features/themes/theme-provider";
 import Footer from "@/components/features/footer/footer";
+import ClickSpark from "@/components/ClickSpark";
 
 export const metadata = {
   title: "Vitruvian Dev",
   description: "Sua empresa fav!",
 };
 
-export default function RootLayout({ children } ) {
+export default function RootLayout({ children }) {
   return (
     <>
       <html lang="pt-br" suppressHydrationWarning>
@@ -20,9 +21,19 @@ export default function RootLayout({ children } ) {
             enableSystem
             disableTransitionOnChange
           >
-            <Navbar/>
-            {children}
-            <Footer/>
+            <ClickSpark
+              sparkColor="#ffffff"
+              sparkSize={10}
+              sparkRadius={15}
+              sparkCount={8}
+              duration={400}
+            >
+              
+              <Navbar />
+              {children}
+              <Footer />
+
+            </ClickSpark>
           </ThemeProvider>
         </body>
       </html>
